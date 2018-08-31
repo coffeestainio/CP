83cab01b11af5da0a847df39f6d88ec38df4bd3a
Pablo Calvo <pablillo@MacBook-Pro-de-2017.local>
2018-08-30 23:26:36 -0600

    task killer

--- FacElec/helpers/AdemarHelper.cs
+++ FacElec/helpers/AdemarHelper.cs
@@ -22,6 +22,8 @@ namespace FacElec.helpers
                 proc.StartInfo.FileName = "C:\\DSign\\ExeFirmaFactura\\FacturaElectronicaCR_CS.exe";
                 proc.StartInfo.Arguments = batCommand;
                 proc.Start();
+                proc.WaitForExit();
+                proc.Kill();
 
             }
             catch (Exception ex){
